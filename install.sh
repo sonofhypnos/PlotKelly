7fe6241200479be282cbf1886309621ed70a669b
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-11-20 13:27:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
+# cd ./PyManifold || echo "PyManifold not found. Please run this in the project root directory of plot-kelly."
 python3 -m venv .venv || echo "Failed to create virtual environment."
-source .venv/bin/activate
-pip install poetry
+source .venv/bin/activate || echo "Failed to activate virtual environment."
+command -v pip3 || echo "pip not found. Please install pip."
+pip3 install poetry || echo "Failed to install poetry in virtual environment."
 poetry install && echo "Install complete. Run run plot-kelly.sh with required arguments."
